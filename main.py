d9e988f650aa6c201d05ebcccf1afe035042f224
Danh Nguyen <65102457+danny-cpp@users.noreply.github.com>
2023-03-01 21:04:29 -0700

    ini

---
+++ main.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+# Debian Package Download and Statistics
+#
+# Danh Nguyen
+# https://github.com/danny-cpp
+# March, 2023
+
+
+if __name__ == '__main__':
+    print("Hi")
+
